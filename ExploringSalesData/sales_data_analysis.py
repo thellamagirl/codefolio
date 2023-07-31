afd677b0da34747927950e78a0e33590cad79de3
thellamagirl <63089370+thellamagirl@users.noreply.github.com>
2023-07-31 16:23:06 -0600

    Updated insert statements to use SQLAlchemy engine

--- ExploringSalesData/sales_data_analysis.py
+++ ExploringSalesData/sales_data_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import mysql.connector
 import settings
+from sqlalchemy import create_engine
 
 # Create Database Connection
 salesdb = mysql.connector.connect(
@@ -50,9 +51,12 @@ mycursor.execute("""
     );
 """)
 
+# Create Database Connection using SQLAlchemy with MySQL Connector backend
+engine = create_engine(f"mysql+mysqlconnector://{settings.USER}:{settings.PASSWORD}@{settings.HOST}/{settings.DATABASE}")
+
 # Read data from online_retail_II.xlsx - Contains 2 sheets: Year 2009-2010 and Year 2010-2011
-df_Y0910 = pd.read_excel('online_retail_II', sheet_name='Year 2009-2010', parse_dates=['InvoiceDate'])
-df_Y1011 = pd.read_excel('online_retail_II', sheet_name='Year 2010-2011', parse_dates=['InvoiceDate'])
+df_Y0910 = pd.read_excel('online_retail_II.xlsx', sheet_name='Year 2009-2010', parse_dates=['InvoiceDate'])
+df_Y1011 = pd.read_excel('online_retail_II.xlsx', sheet_name='Year 2010-2011', parse_dates=['InvoiceDate'])
 
 # Combine data into single df
 df_combined = pd.concat([df_Y0910, df_Y1011], ignore_index=True)
@@ -67,8 +71,11 @@ customers_column_mapping = {
 df_customers = df_combined[['Customer ID', 'Country']]
 # Remove duplicate customers if needed
 df_customers.drop_duplicates(subset='Customer ID', inplace=True) 
-# Convert to sql and insert into 'customers' table in 'salesdb'
-df_customers.to_sql('customers', salesdb, if_exists='append', index=False, columns=customers_column_mapping)
+# Rename columns of df_customers using customers_column_mapping
+df_customers.rename(columns=customers_column_mapping, inplace=True)
+
+# Convert to sql and insert into 'customers' table in 'salesdb' using SQLAlchemy engine
+df_customers.to_sql('customers', con=engine, if_exists='append', index=False)
 
 # Column mapping df to 'products' table
 products_column_mapping = {
@@ -80,8 +87,11 @@ products_column_mapping = {
 df_products = df_combined[['StockCode', 'Description']]
 # Remove duplicate products if needed
 df_products.drop_duplicates(subset='StockCode', inplace=True)
-# Convert to sql and insert into 'products' table in 'salesdb'
-df_products.to_sql('products', salesdb, if_exists='append', index=False, columns=products_column_mapping)
+# Rename columns of df_products using products_column_mapping
+df_products.rename(columns=products_column_mapping, inplace=True)
+
+# Convert to sql and insert into 'products' table in 'salesdb' using SQLAlchemy engine
+df_products.to_sql('products', con=engine, if_exists='append', index=False)
 
 # Column mapping df to 'sales' table
 sales_column_mapping = {
@@ -97,6 +107,9 @@ sales_column_mapping = {
 
 # Insert data into 'sales' table using column mapping
 df_sales = df_combined[['Invoice','StockCode', 'Description', 'Quantity', 'InvoiceDate', 'Price', 'Customer ID', 'Country']]
-# Convert to sql and insert into 'sales' table in 'salesdb'
-df_sales.to_sql('sales', salesdb, if_exists='append', index=False, columns=sales_column_mapping)
+# Rename columns of df_sales using sales_column_mapping
+df_sales.rename(columns=sales_column_mapping, inplace=True)
+
+# Convert to sql and insert into 'sales' table in 'salesdb' using SQLAlchemy engine
+df_sales.to_sql('sales', con=engine, if_exists='append', index=False)
 
