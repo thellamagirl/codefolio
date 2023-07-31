1c8eccc7122d0008be628033419e78e20cc95fdc
thellamagirl <63089370+thellamagirl@users.noreply.github.com>
2023-07-31 14:49:59 -0600

    Add table creation blocks for customers, sales, product tables

--- ExploringSalesData/sales_data_analysis.py
+++ ExploringSalesData/sales_data_analysis.py
@@ -1,17 +1,51 @@
-# Imports
 import pandas as pd
 import numpy as np
 import mysql.connector
 import settings
 
-# Create Database
+# Create Database Connection
 salesdb = mysql.connector.connect(
-  host=settings.HOST,
-  user=settings.USER,
-  password=settings.PASSWORD
+    host=settings.HOST,
+    user=settings.USER,
+    password=settings.PASSWORD
 )
 
 mycursor = salesdb.cursor()
 
-mycursor.execute("CREATE DATABASE salesdatabase")
+# Create the 'salesdb' database
+mycursor.execute("CREATE DATABASE IF NOT EXISTS salesdb")
 
+
+# Select 'salesdb' to use during table creation
+mycursor.execute("USE salesdb")
+
+
+# Create tables
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS customers (
+        CustomerID INT PRIMARY KEY, 
+        Country VARCHAR(100)
+    );                 
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        StockCode VARCHAR(50) PRIMARY KEY,
+        Description VARCHAR(255)
+    );
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS sales (
+        Invoice INT PRIMARY KEY,
+        StockCode VARCHAR(50),
+        Description VARCHAR(255),
+        Quantity INT,
+        InvoiceDate DATETIME,
+        Price FLOAT,
+        CustomerID INT,
+        Country VARCHAR(50),
+        FOREIGN KEY (CustomerID) REFERENCES customers(CustomerID),
+        FOREIGN KEY (StockCode) REFERENCES products(StockCode)
+    );
+""")
